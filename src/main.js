31688ab573b47cbc8914331480399f28045b619f
luvale <valeriae200@hotmail.com>
2021-06-22 08:46:22 -0500

    import ghibli

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
+// importamos data del Studio Ghibli;
 
 console.log(example, data);
