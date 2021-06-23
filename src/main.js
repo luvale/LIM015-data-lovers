80eb3ed796002311cf5d1305bf19741ac5692b96
DianaHuaripayta <angelicagonzales1610@gmail.com>
2021-06-23 11:35:47 -0500

    showData

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import { example } from './data.js';
+import { showData } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 // importamos data del Studio Ghibli;
-
-console.log(example, data);
+showData(data);
