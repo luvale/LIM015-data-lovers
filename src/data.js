e7c60219e1d41b8af4623edeafa4874312a17535
luvale <valeriae200@hotmail.com>
2021-06-28 09:56:49 -0500

    intentos de Val

--- src/data.js
+++ src/data.js
@@ -1,13 +1,37 @@
 // estas funciones son de ejemplo
 import data from './data/ghibli/ghibli.js';
 
+/*
+export const showProducers =
+data.films.map( function (all) {
+  return all.director
+});
+console.log(showProducers);
+*/
+
+export const descriptions = (data) => {
+let pelis = data.films.map(a => (a.people));
+console.log(pelis[0][0]);
+}
+
+/*
+Object.entries(data.films);
+console.log(pelis[0][1])
+*/
+
+ /* export const showProducers = data.films.map(a => (a.producer));
+console.log(showProducers);
+*/
+
+
+/*
 export const showData = () => {
   data.films.forEach(function(element) {
    console.log(element);
 })
   // console.log(a);
 }
-
+*/
 /* export const example = () => {
   return 'example';
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { showData } from './data.js';
+import { descriptions } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 // importamos data del Studio Ghibli;
-showData(data);
+descriptions(data);
