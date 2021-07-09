40aa3ec6f0e069e57fb2bea0d9c24430ba463b8e
luvale <valeriae200@hotmail.com>
2021-07-09 12:47:50 -0500

    estilos de botones y sortbydirectors

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+import data from './data/ghibli/ghibli.js';
 // Trae Films
 export const getFilm = (data) => {
   const dataGhibli = data.films.map((arr) => arr);
@@ -10,9 +11,20 @@ export const searchYears = (arrayData, release_date) => {
   return result;
 }
 
-// Exporto a todos los productores
-export const allProducers = (data) => {
-  const arrayP = data.films.map(a => (a.producer));
+// Hago un array de todos los productores
+ export const mapDirectors = (data) => {
+  const arrayP = data.films.map(a => (a.director));
   return arrayP;
 }
 
+const arrayDirectors = mapDirectors(data);
+// console.log (arrayProducers)
+// Filtro el array de producers para que los nombres no se repitan
+export const directors = arrayDirectors.filter((value, index) =>{
+  return arrayDirectors.indexOf(value) === index;
+});
+
+export const filterDirectors = (data, value) => {
+  const filterDirectors = data.films.filter(x => x.director === value)
+  return filterDirectors;  
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {getFilm, searchYears, allProducers} from './data.js';
+import {getFilm, searchYears, directors , filterDirectors } from './data.js';
 import data from './data/ghibli/ghibli.js';
 const filmsGhibli = getFilm(data);//obtiene la data Original y la copia seria filmGhibli
 // trae solo films.
@@ -9,7 +9,7 @@ const showAllFilms = document.getElementById("showAllFilms")
 function showFilmsInScreen (arrayData){
     arrayData.forEach(element => {
         const divFilm = document.createElement("section");
-        divFilm.innerHTML=`<p>Title: ${element.title}</p>
+        divFilm.innerHTML=`<p>${element.title}</p>
         <img src="${element.poster}" alt="">`
         showAllFilms.append(divFilm);
         divFilm.classList.add("cardClass");
@@ -22,30 +22,32 @@ const dropdown = (arrayData) =>{
      arrayData.forEach((element) => {
          const optionYears = document.createElement("option");
          optionYears.innerHTML= `${element.release_date}`;
-         console.log(optionYears);
+         
          selectDropdown.appendChild(optionYears);
      });
       return showAllFilms;
 };
 dropdown(filmsGhibli);
 /* Filter(SORT) BY years --> Diana */
-
+const select = document.getElementById("directors");
 /* Sort by producers --> Valeria */
-const arrayProducers = allProducers(data)
-
-// Filtro el array de producers para que los nombres no se repitan
-const producers = arrayProducers.filter((value, index) =>{
-    return arrayProducers.indexOf(value) === index;
-});
-
 //Uso la nueva lista de producers en un <select>
-function sortByProducer() {
-    producers.forEach( element => {
+function directorsList() {
+    directors.forEach( element => {
         const options = document.createElement("option");
-        options.innerHTML= `${element}`;
-        const select = document.getElementById("producers");
+        options.value = element;
+        options.textContent= `${element}`;
         select.append(options);
     })
 }
-sortByProducer(filmsGhibli);
-/* Sort by producers --> Valeria */
+directorsList(filmsGhibli);
+
+select.addEventListener("change", sortByDirectors);
+
+function sortByDirectors() {
+    if (select.value !== "Sort By Directors"){
+        showAllFilms.textContent= "";
+        const selectDirector = filterDirectors(data, select.value);
+        showFilmsInScreen(selectDirector);
+    }     
+}
