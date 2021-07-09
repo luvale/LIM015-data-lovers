7e3afa1034ec588e03493040d9c94afd875ea67e
DianaHuaripayta <angelicagonzales1610@gmail.com>
2021-07-09 12:50:53 -0500

    sortByYears-arrObj-ForEach-Push(Date)[]

--- src/main.js
+++ src/main.js
@@ -1,51 +1,54 @@
-import {getFilm, searchYears, allProducers} from './data.js';
 import data from './data/ghibli/ghibli.js';
+import {getFilm, searchYears} from './data.js';
 const filmsGhibli = getFilm(data);//obtiene la data Original y la copia seria filmGhibli
 // trae solo films.
-// Muestra todas las tarjetas
+const showAllFilms = document.getElementById("showAllFilms");
+const elementFilterYears = document.querySelector("#filter-years");
 
-const showAllFilms = document.getElementById("showAllFilms")
-
-function showFilmsInScreen (arrayData){
-    arrayData.forEach(element => {
-        const divFilm = document.createElement("section");
-        divFilm.innerHTML=`<p>Title: ${element.title}</p>
-        <img src="${element.poster}" alt="">`
-        showAllFilms.append(divFilm);
-        divFilm.classList.add("cardClass");
-    });
-}
-showFilmsInScreen(filmsGhibli);
-
-/* Filter(SORT) BY years --> Diana (traer ID - incluir funcion)*/
-const dropdown = (arrayData) =>{
+const showFilmInScreen = (arrayData) =>{
      arrayData.forEach((element) => {
-         const optionYears = document.createElement("option");
-         optionYears.innerHTML= `${element.release_date}`;
-         console.log(optionYears);
-         selectDropdown.appendChild(optionYears);
+         const divFilm = document.createElement("section");
+         divFilm.classList.add("cardClass");
+         divFilm.innerHTML=`<p>${element.title}</p>
+         <img src="${element.poster}" alt="">`;
+         showAllFilms.appendChild(divFilm);
      });
-      return showAllFilms;
-};
-dropdown(filmsGhibli);
-/* Filter(SORT) BY years --> Diana */
+     return showAllFilms;
+ };
+ showFilmInScreen(filmsGhibli);
+ 
+/* DROPDOWN-Years*/
+const withoutDuplicateYears = (arr) => {
+  let result = arr.filter((item,index)=>{
+    return arr.indexOf(item) === index;
+  });
+  return result;
+}
+const arrDate = (arrObj) => {
+  let newArray = [];
+  arrObj.forEach((ele) => {    
+      newArray.push(ele.release_date);
+  });
+  return newArray;
+}
+const onlyYears = arrDate(filmsGhibli);
+const nameYears = withoutDuplicateYears(onlyYears);
+console.log(withoutDuplicateYears(onlyYears));
 
-/* Sort by producers --> Valeria */
-const arrayProducers = allProducers(data)
-
-// Filtro el array de producers para que los nombres no se repitan
-const producers = arrayProducers.filter((value, index) =>{
-    return arrayProducers.indexOf(value) === index;
+nameYears.forEach(name => {
+  const newOption = document.createElement('option');
+  newOption.value= name;
+  newOption.textContent = name;
+  elementFilterYears.appendChild(newOption)
 });
 
-//Uso la nueva lista de producers en un <select>
-function sortByProducer() {
-    producers.forEach( element => {
-        const options = document.createElement("option");
-        options.innerHTML= `${element}`;
-        const select = document.getElementById("producers");
-        select.append(options);
-    })
-}
-sortByProducer(filmsGhibli);
-/* Sort by producers --> Valeria */
+elementFilterYears.addEventListener('change',() =>{
+  if(elementFilterYears.value === 'all'){
+    showAllFilms.innerHTML = '';
+    showFilmInScreen(filmsGhibli);
+  } else{
+    const catchFilter = searchYears(filmsGhibli, elementFilterYears.value);
+    showAllFilms.innerHTML='';
+    showFilmInScreen(catchFilter);
+  }
+})
