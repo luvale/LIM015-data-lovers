2b4472de7c1db41475d1a8cfb553291619d5abdd
luvale <valeriae200@hotmail.com>
2021-07-26 23:15:56 -0500

    test

--- test/data.spec.js
+++ test/data.spec.js
@@ -5,15 +5,15 @@ describe('getFilm', () => {
     expect(typeof getFilm).toBe('function');
   });
   it('Muestra toda la información', () => {
-    const example = {"films":[
-      {director:"Hayao Miyazaki", title:"Castle in the Sky"}, 
-      {director: "Isao Takahata" , title: "Grave of the Fireflies"},
-      {director: "Hayao Miyazaki" , title: "My Neighbor Totoro"}
+    const example = { "films" :[
+      {"director":"Hayao Miyazaki", "title":"Castle in the Sky"},  // en realidad los keys no necesitan comillas si no tienen espacios
+      {"director": "Isao Takahata" , "title": "Grave of the Fireflies"},
+      {"director": "Hayao Miyazaki" , "title": "My Neighbor Totoro"}
     ]}
       expect(getFilm(example)).toEqual([
-        {director:"Hayao Miyazaki", title:"Castle in the Sky"}, 
-        {director: "Isao Takahata" , title: "Grave of the Fireflies"},
-        {director: "Hayao Miyazaki" , title: "My Neighbor Totoro"}
+        {"director":"Hayao Miyazaki", "title":"Castle in the Sky"}, 
+        {"director": "Isao Takahata" , "title": "Grave of the Fireflies"},
+        {"director": "Hayao Miyazaki" , "title": "My Neighbor Totoro"}
       ])
     })
 });
@@ -31,21 +31,21 @@ describe('filterDirectors' , () => {
   })
   it ('Movies by `Hayao Miyazaki`', () => {
     const example = {"films":[
-      {director:"Hayao Miyazaki", title:"Castle in the Sky"}, 
-      {director: "Isao Takahata" , title: "Grave of the Fireflies"}
+      {"director":"Hayao Miyazaki", "title":"Castle in the Sky"}, 
+      {"director": "Isao Takahata" , "title": "Grave of the Fireflies"}
     ]}
-      expect(filterDirectors(example, "Hayao Miyazaki")).toEqual([{director:"Hayao Miyazaki", title:"Castle in the Sky"}])
+      expect(filterDirectors(example, "Hayao Miyazaki")).toEqual([{"director":"Hayao Miyazaki", "title":"Castle in the Sky"}])
   })
 })
 
-describe('searchYears' , () => {
+describe('searchYears'  , () => {
   it ('`searchYears` is a function', () => {
     expect(typeof searchYears).toBe('function');
   })
   it ('Movies from 1986', () => {
-    const example =  [{release_date: "1986", title: "Castle in the Sky"}, 
-    {release_date: "1988", title: "My Neighbor Totoro",}]
-    expect(searchYears(example , "1986")).toEqual([{release_date: "1986", title: "Castle in the Sky"}])
+    const example =  [{"release_date": "1986", "title": "Castle in the Sky"}, 
+    {"release_date": "1988", "title": "My Neighbor Totoro",}]
+    expect(searchYears(example , "1986")).toEqual([{"release_date": "1986", "title": "Castle in the Sky"}])
   })
 })
 
@@ -55,14 +55,14 @@ describe('sortAZ', () => {
   });
   it('Ordena de la A a la Z', () => {
     const example = [
-      {title:"Castle in the Sky"}, 
-      {title: "My Neighbor Totoro"},
-      {title: "Grave of the Fireflies"}
+      {"title":"Castle in the Sky"}, 
+      {"title": "My Neighbor Totoro"},
+      {"title": "Grave of the Fireflies"}
     ]
       expect(sortAZ(example)).toEqual([
-        {title:"Castle in the Sky"}, 
-        {title: "Grave of the Fireflies"},
-        {title: "My Neighbor Totoro"}
+        {"title":"Castle in the Sky"}, 
+        {"title": "Grave of the Fireflies"},
+        {"title": "My Neighbor Totoro"}
       ])
     })
 });
@@ -73,14 +73,14 @@ describe('sortZA', () => {
   });
   it('Ordena de la Z a la A', () => {
     const example = [
-      {title:"Castle in the Sky"}, 
-      {title: "My Neighbor Totoro"},
-      {title: "Grave of the Fireflies"}
+      {"title":"Castle in the Sky"}, 
+      {"title": "My Neighbor Totoro"},
+      {"title": "Grave of the Fireflies"}
     ]
       expect(sortZA(example)).toEqual([
-        {title: "My Neighbor Totoro"},
-        {title: "Grave of the Fireflies"},
-        {title:"Castle in the Sky"}
+        {"title": "My Neighbor Totoro"},
+        {"title": "Grave of the Fireflies"},
+        {"title":"Castle in the Sky"}
       ])
     })
 });
